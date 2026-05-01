7ee36944ca0da29b058afe17252206871a45f804
rogtavares <rogerio@almeidatavares.com.br>
2026-05-01 02:15:43 -0300

    feat:refactor CSS e JS para arquivos separados (styles.css, app.js)

--- tests/hello-world.spec.ts
+++ tests/hello-world.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('hello world', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto('http://localhost:8000');
     const helloWorldText = await page.textContent('h1');
     expect(helloWorldText).toBe('Hello World');
 });
